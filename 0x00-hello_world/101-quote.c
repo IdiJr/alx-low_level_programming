0f9ba8d39ad3d7abb0f3fd81dbb661ad40e15896
IdiJr <idialijnr@gmail.com>
2023-02-16 18:02:22 -0800

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry point
- *
- * Return: Always 0 (success)
+ * Return: Always 1 if success
  */
 int main(void)
 {
-fputs("and that piece of art is useful\n" -  Dora Korpar, 2015 - 10 - 19, stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
