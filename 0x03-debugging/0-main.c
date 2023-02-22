2558c263c0afda40d221f649eaea9f26ed27c74f
IdiJr <idialijnr@gmail.com>
2023-02-22 01:39:23 -0800

    update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - Test function for positive or negative integer
- * Return: 0 on success
+ * main - Test function for positive or negative
+ * Return: 0 
  */
 
 int main(void)
@@ -11,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+	
 	return (0);
 }
