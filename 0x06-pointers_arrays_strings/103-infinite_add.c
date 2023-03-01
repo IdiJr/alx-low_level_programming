bb278250dacd8168915eaaf9ca4107c35a387132
IdiJr <idialijnr@gmail.com>
2023-03-01 23:05:14 +0000

    update of 0x06-pointer_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -35,38 +35,40 @@ void rev_string(char *n)
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, i = 0, j = 0;
-	int a = 0, b = 0, str = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	while (*(n1 + len2) != '\0')
-		len2++;
-	while (*(n2 + i) != '\0')
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
 		j++;
-	len2--;
 	i--;
-	if (i >= size_r || len2 >= size_r)
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-	while (i >= 0 || len2 >= 0 || len1 == 1)
+	while (j >= 0 || i >= 0 || overflow == 0)
 	{
 		if (i < 0)
-		a = 0;
+			val1 = 0;
 		else
-			b = *(n2 + i) - '0';
-		str = a + b + len1;
-		if (str >= 10)
-			len1 = 1;
+			val1 = *(n1 + i) - '0';
+		if (j == 0)
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
 		else
-			len1 = 0;
-		if (j >= (size_r - 1))
+			overflow = 0;
+		if (digits >= (size_r - 1))
 			return (0);
-		*(r + j) = (str % 10) + '0';
-		j++;
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
 		i--;
-		len2--;
 	}
-	if (j == size_r)
+	if (digits == size_r)
 		return (0);
-	*(r + j) = '\0';
+	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
 }
