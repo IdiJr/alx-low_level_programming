6b6d4f9fb39a1580653d2f0d9cc7b80eae29b07c
IdiJr <idialijnr@gmail.com>
2023-04-04 15:58:22 +0000

    Reverse list

--- 0x13-more_singly_linked_lists/100-main.c
+++ 0x13-more_singly_linked_lists/100-main.c
@@ -11,7 +11,6 @@
 int main(void)
 {
 	listint_t *head;
-	int n;
 
 	head = NULL;
 	add_nodeint_end(&head, 0);
@@ -23,7 +22,7 @@ int main(void)
 	add_nodeint_end(&head, 402);
 	add_nodeint_end(&head, 1024);
 	print_listint(head);
-	reverse_listint(head);
+	reverse_listint(&head);
 	print_listint(head);
 	free_listint2(&head);
 	return (0);
